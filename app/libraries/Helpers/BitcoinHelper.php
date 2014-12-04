29196eac7e54c1053c75e01dd73fba2db4c3d74e
Artur Schaback <skyzer@gmail.com>
2014-12-04 16:54:13 -0500

    ignore scientific notation for small btc amounts

--- app/libraries/Helpers/BitcoinHelper.php
+++ app/libraries/Helpers/BitcoinHelper.php
@@ -6,7 +6,7 @@ class BitcoinHelper {
 
 	public static function satoshiToBtc($satoshi)
 	{
-		return (float)bcdiv($satoshi, SATOSHI_FRACTION, 8);
+		return rtrim(sprintf('%.20F', bcdiv($satoshi, SATOSHI_FRACTION, 8)), '0');
 	}
 
 	public static function validateBitcoinAddress($addr) {
